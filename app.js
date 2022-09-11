e0db7ce7f392eb192a1e099ae0a426b996eb092c
Lauren McKeown <lauren.makenna.mck@gmail.com>
2022-09-11 17:56:33 -0400

    pushing lab

--- app.js
+++ app.js
@@ -26,15 +26,22 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const sortfruit = fruitsArray.filter(fruits => fruits.nutritions.sugar > 15)
+
+  const newfruit = sortfruit.map(fruit => fruit.name).join(", ");
   // TODO: Create a new HTML element to display your data
+  const newlst = document.createElement('div');
+  newlst.innerHTML =  newfruit;
+
 
   // TODO: Append your new element to the page
-
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newlst);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
